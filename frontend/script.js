db15c2c54ee19ddaa8d482520b81eb991743baa2
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-09-18 19:49:17 +0530

    modified frontend,added more contents,modified booking

--- frontend/script.js
+++ frontend/script.js
@@ -9,3 +9,21 @@ window.addEventListener('scroll', function() {
         navbar.style.boxShadow = '0 2px 10px rgba(0,0,0,0.05)';
     }
 });
+
+// Open the popup
+function openPopup() {
+    document.getElementById('bookingPopup').style.display = 'flex';
+}
+
+// Close the popup
+function closePopup() {
+    document.getElementById('bookingPopup').style.display = 'none';
+}
+
+// Close the popup if the user taps the dark background outside the white box
+window.onclick = function(event) {
+    let popup = document.getElementById('bookingPopup');
+    if (event.target == popup) {
+        popup.style.display = 'none';
+    }
+}
